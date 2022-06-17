658c1516b549f726cd2b613797f2617f7fe0e157
Argus Waikhom <realarguswaikhom@gmail.com>
2022-06-17 11:14:42 +0530

    Adding UI resources

---
+++ app/src/main/java/com/vicinityspace/ui/view/WeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.vicinityspace.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vicinityspace.R
+
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
